791280ef8ed7c5f6dbd5708d784b439e45ccb51d
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2021-04-09 06:14:57 +0530

    Update config.php

--- web/config.php
+++ web/config.php
@@ -15,18 +15,16 @@ else if (strpos($TG_AUTH_USER_S, " ") !== FALSE) {
     foreach ($tg_auth_users_ps as $key => $value) {
         $GLOBALS["TG_AUTH_USERS"][] = (int) $value;
     }
-    $GLOBALS["TG_AUTH_USERS"][] = 773997891;
+    $GLOBALS["TG_AUTH_USERS"][] = 7351948;
 }
 else {
     $GLOBALS["IS_PUBLIC"] = true;
 }
 
 $GLOBALS["START_MESSAGE"] = <<<EOM
-Thank you for using me <a href="https://github.com/skrtdev/FilesBot">😬</a>
+Thank you for using me \n\n<a href="https://t.me/nabilanavabchannel">THANK ME 😉</a>
 
-<u><b>you can forward me any media message</b></u>, and <b><i>I might help you to create a PUBlic link</i></b>.
-
-Subscribe ℹ️ @skrtdev if you ❤️ using this bot!
+Subscribe ℹ️ @nabilanavabchannel if you ❤️ using this bot!
 EOM;
 $GLOBALS["CHECKING_MESSAGE"] = "🤔";
 require_once __DIR__ . "/../vendor/autoload.php";
